1c231b5e8a6dd5af20754fa109f6cf07b3fa2294
Peter Pilgerstorfer <peter@pilgerstorfer.at>
2012-10-30 03:09:38 +0100

    Zusicherungen GuV

--- src/GuV.java
+++ src/GuV.java
@@ -1,17 +1,12 @@
 import java.util.List;
 
-
 /**
+ * Invariante: band ist ungleich NULL
  * 
  * @author Christian Kletzander
- * 
  */
 
 public class GuV extends Selection<Posten> {
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	private Band band;
@@ -19,9 +14,10 @@ public class GuV extends Selection<Posten> {
 	/**
 	 * NOTE: Erstellt ein neues Objekt GuV fuer die Klasse Band.
 	 * 
-	 * Vorbedingung: band darf nicht NULL sein.
+	 * Vorbedingung: band ist ungleich NULL
 	 * 
-	 * @param band - Ein Band Objekt fuer das dieses GuV Objekt erstellt wird.
+	 * @param band
+	 *            - Ein Band Objekt fuer das dieses GuV Objekt erstellt wird.
 	 */
 	public GuV(Band band) {
 		this.band = band;
@@ -32,10 +28,13 @@ public class GuV extends Selection<Posten> {
 	 * <code>base</code> arbeitet. Es sind jedoch nur Elemente sichtbar, die von
 	 * den Selektoren selektiert werden.
 	 * 
-	 * Vorbedinung: band darf nicht NULL sein. base darf nicht NULL sein.
+	 * Vorbedingung: band, base und selectors sind ungleich NULL, selectors
+	 * enthaelt keine Elemente gleich NULL
 	 * 
-	 * @param base - Das auszugehende GuV Objekt.
-	 * @param selectors - Selektoren, die Suchbedingungen setzen.
+	 * @param base
+	 *            Das auszugehende GuV Objekt.
+	 * @param selectors
+	 *            Selektoren, die Suchbedingungen setzen.
 	 */
 	private GuV(Band band, GuV base, List<Selector<Posten>> selectors) {
 		super(base, selectors);
@@ -43,12 +42,16 @@ public class GuV extends Selection<Posten> {
 	}
 
 	/**
-	 * NOTE: Liefert eine Selektion der in diesem Objekt gespeicherten Posten. Mit
-	 * den uebergebenen Selektoren kann bestimmt werden, welche Posten
+	 * NOTE: Liefert eine Selektion der in diesem Objekt gespeicherten Posten.
+	 * Mit den uebergebenen Selektoren kann bestimmt werden, welche Posten
 	 * selektiert werden. Aenderungen in der zurueckgegebenen Selektion wirken
 	 * sich direkt auf das Original aus.
 	 * 
-	 * @param selectors - Selektoren, die Suchbedingungen setzen.
+	 * Vorbedingung: selectors ist ungleich NULL und enthaelt keine Elemente
+	 * gleich NULL
+	 * 
+	 * @param selectors
+	 *            - Selektoren, die Suchbedingungen setzen.
 	 * @return - Gibt ein GuV-Objekt mit gesetzten Filtern zurueck.
 	 */
 	public GuV select(List<Selector<Posten>> selectors) {
@@ -56,10 +59,10 @@ public class GuV extends Selection<Posten> {
 	}
 
 	/**
-	 * NOTE: Berechnet die Summe der Einnahmen aller selektierter Posten. Daber werden
-	 * auch Posten der Termine beruecksichtigt.
+	 * NOTE: Berechnet die Summe der Einnahmen aller selektierter Posten. Daber
+	 * werden auch Posten der Termine beruecksichtigt.
 	 * 
-	 * Nachbedingung: 0 <= RŸckgabewert <= +unendlich
+	 * Nachbedingung: der Rueckgabewert ist >= 0
 	 * 
 	 * @return Summe der Einnahmen der selektierten Posten
 	 */
@@ -67,16 +70,19 @@ public class GuV extends Selection<Posten> {
 		double einnahmen = 0;
 
 		/*
-		 * NOTE: Durchiterieren der gespeicherten Posten.
-		 * 		 Falls keine Posten vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren der gespeicherten Posten. Falls keine Posten
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Posten posten : this) {
 			einnahmen += posten.getEinnahmen();
 		}
 
+		// Zusicherung: einnahmen enthaelt die Summe der Einnahmen aller
+		// selektierten Posten (ohne Termine)
+
 		/*
-		 * NOTE: Durchiterieren aller Termine der Band. 
-		 * 		 Falls keine Termine vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren aller Termine der Band. Falls keine Termine
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Termin termin : band.getTermine()) {
 			Posten posten = termin.getPosten();
@@ -89,10 +95,10 @@ public class GuV extends Selection<Posten> {
 	}
 
 	/**
-	 * NOTE: Berechnet die Summe der Ausgaben aller selektierter Posten. Daber werden
-	 * auch Posten der Termine beruecksichtigt.
+	 * NOTE: Berechnet die Summe der Ausgaben aller selektierter Posten. Daber
+	 * werden auch Posten der Termine beruecksichtigt.
 	 * 
-	 * Nachbedingung: -unendlich <= RŸckgabewert <= 0
+	 * Nachbedingung: der Rueckgabewert ist >= 0
 	 * 
 	 * @return Summe der Ausgaben der selektierten Posten
 	 */
@@ -100,16 +106,19 @@ public class GuV extends Selection<Posten> {
 		double ausgaben = 0;
 
 		/*
-		 *  NOTE: Durchiterieren der gespeicherten Posten
-		 *  	  Falls keine Posten vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren der gespeicherten Posten Falls keine Posten
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Posten posten : this) {
 			ausgaben += posten.getAusgaben();
 		}
 
+		// Zusicherung: ausgaben enthaelt die Summe der Ausgaben aller
+		// selektierten Posten (ohne Termine)
+
 		/*
-		 *  NOTE: Durchiterieren aller Termine der Band
-		 *  	  Falls keien Termine vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren aller Termine der Band Falls keien Termine
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Termin termin : band.getTermine()) {
 			Posten posten = termin.getPosten();
@@ -122,10 +131,8 @@ public class GuV extends Selection<Posten> {
 	}
 
 	/**
-	 * NOTE: Berechnet den Gesamtgewinn aller selektierter Posten. Daber werden auch
-	 * Posten der Termine beruecksichtigt.
-	 * 
-	 * Nachbedingung: -unendlich <= RŸckgabewert <= +unendlich
+	 * NOTE: Berechnet den Gesamtgewinn aller selektierter Posten. Daber werden
+	 * auch Posten der Termine beruecksichtigt.
 	 * 
 	 * @return Gesamtgewinn der selektierten Posten
 	 */
@@ -133,16 +140,19 @@ public class GuV extends Selection<Posten> {
 		double ausgaben = 0;
 
 		/*
-		 * NOTE: Durchiterieren der gespeicherten Posten
-		 * 		 Falls keien Posten vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren der gespeicherten Posten Falls keien Posten
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Posten posten : this) {
 			ausgaben += posten.getEinnahmen() - posten.getAusgaben();
 		}
 
+		// Zusicherung: ausgaben enthaelt den Gesamtgewinn aller
+		// selektierter Posten (ohne Termine)
+
 		/*
-		 * NOTE: Durchiterieren aller Termine der Band
-		 * 		 Falls keine Termine vorhanden sind, wird dieser Block uebersprungen.
+		 * NOTE: Durchiterieren aller Termine der Band Falls keine Termine
+		 * vorhanden sind, wird dieser Block uebersprungen.
 		 */
 		for (Termin termin : band.getTermine()) {
 			Posten posten = termin.getPosten();
